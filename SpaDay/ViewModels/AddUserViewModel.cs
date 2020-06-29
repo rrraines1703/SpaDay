3de540e98718793b6168b1adc45004a0dc56983e
Richard Raines <rrraines1703@me.com>
2020-06-28 23:18:43 -0400

    Completed Studio 15

---
+++ SpaDay/ViewModels/AddUserViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SpaDay.ViewModels
+{
+    public class AddUserViewModel
+    {
+        [Required(ErrorMessage = "Username must be between 5 and 15 characters.")]
+        [StringLength(15, MinimumLength = 5)]
+        public string Username { get; set; }
+
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Password must be between 6 and 20 characters")]
+        [StringLength(20, MinimumLength = 6)]
+        public string Password { get; set; }
+
+        [Required]
+        public string VerifyPassword { get; set; }
+    }
+}
